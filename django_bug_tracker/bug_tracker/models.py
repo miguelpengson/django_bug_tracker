ce16a271824f1d0b73ef1eddd3ebb2ffba1a3eb1
miguelpengson <miguelpengson@gmail.com>
2022-05-22 01:10:29 +0800

    first migration, created admin user

--- django_bug_tracker/bug_tracker/models.py
+++ django_bug_tracker/bug_tracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User # Need to check this
 
-# Create your models here.
+    
+class Bug(models.Model):
+    # User and bugs created get deleted when remove
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    summary = models.TextField() # Check this TestField max_length?
+    # Other ways to do this (priority), first test
+    PRIORITY_CHOICES = [('LOW', 'Low'), ('MEDIUM', 'Medium'), ('HIGH', 'High')]
+    priority = models.CharField(max_length=6, choices= PRIORITY_CHOICES, default='LOW')
+    
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.bug_text
+    
+    
--- django_bug_tracker/django_bug_tracker/settings.py
+++ django_bug_tracker/django_bug_tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bug_tracker.apps.BugTrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
