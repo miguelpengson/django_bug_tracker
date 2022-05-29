49970babc27b43459e490f03b0d75780fac06167
miguelpengson <miguelpengson@gmail.com>
2022-05-29 11:36:07 +0800

    created users app, ugly but working

--- django_bug_tracker/django_bug_tracker/settings.py
+++ django_bug_tracker/django_bug_tracker/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bug_tracker.apps.BugTrackerConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
