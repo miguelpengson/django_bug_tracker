f5f1660f88446720af61ca28b6f6918497eed1f5
miguelpengson <miguelpengson@gmail.com>
2022-05-31 10:12:39 +0800

    extended user registerform, added email address

--- django_bug_tracker/users/views.py
+++ django_bug_tracker/users/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(request):
     # Check the validity of the entered items
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('index')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
                
     return render(request, 'users/register.html', {'form': form})
