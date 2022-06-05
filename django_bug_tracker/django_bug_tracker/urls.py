78cdfca13f1080ac5358f60b28a9f933cd7ec2d3
miguelpengson <miguelpengson@gmail.com>
2022-06-05 18:58:38 +0800

    fixed some urlConfs and static files

--- django_bug_tracker/django_bug_tracker/urls.py
+++ django_bug_tracker/django_bug_tracker/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     path('', include('bug_tracker.urls')),
     path('bug_tracker/', include('bug_tracker.urls')),
     path('register/', include('users.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
 ]
